819bd4bdacd8b054691a5b5b365fe99e0be66040
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-08 00:30:07 +0200

    update

---
+++ 2-HTML5&CSS3/Day2/Tasks/script.js
@@ -0,0 +1,8 @@
+const contactChip = document.querySelector('.contact-chip');
+
+const close = () =>{
+    contactChip.style.width = "0%";
+}
+const open = () =>{
+    contactChip.style.width = "100%";
+}
